d6471342969fa7b2fba5dde5d6d0976534d85e2a
Remy Schrader <remy@rootandflow.com>
2016-10-18 14:51:49 -0700

    removes console.log test message

--- js/site.bundle.js
+++ js/site.bundle.js
@@ -53,7 +53,11 @@
 /* 3 */
 /***/ function(module, exports) {
 
-	console.log('site script loaded');
+	/**
+	 * @license Asparagus v1.0
+	 * (c) 2013 Form5 http://form5.is
+	 * License: MIT
+	 */
 
 
 /***/ }
