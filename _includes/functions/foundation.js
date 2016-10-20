c4dd3b4d64a432d2fb8414d7b1720a7dceb74b66
Remy Schrader <remy@rootandflow.com>
2016-10-20 13:11:43 -0700

    :v: hotfixes logic syntax for Interchange retina media queries breakpoints

--- _includes/functions/foundation.js
+++ _includes/functions/foundation.js
@@ -1,19 +1,17 @@
-Foundation.Interchange.SPECIAL_QUERIES['medium_retina'] = 'only screen \
-  and (min-width: 40em) \
-  and (-webkit-min-device-pixel-ratio: 2), \
-  only screen and (min--moz-device-pixel-ratio: 2), \
-  only screen and (-o-min-device-pixel-ratio: 2/1), \
-  only screen and (min-device-pixel-ratio: 2), \
-  only screen and (min-resolution: 192dpi), \
-  only screen and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['medium_retina'] = 'only screen ' +
+  'and (-webkit-min-device-pixel-ratio: 2) and (min-width: 40em), ' +
+  'only screen and (min--moz-device-pixel-ratio: 2) and (min-width: 40em), ' +
+  'only screen and (-o-min-device-pixel-ratio: 2/1) and (min-width: 40em), ' +
+  'only screen and (min-device-pixel-ratio: 2) and (min-width: 40em), ' +
+  'only screen and (min-resolution: 192dpi) and (min-width: 40em), ' +
+  'only screen and (min-resolution: 2dppx) and (min-width: 40em)';
 
-Foundation.Interchange.SPECIAL_QUERIES['large_retina'] = 'only screen \
-  and (min-width: 64em) \
-  and (-webkit-min-device-pixel-ratio: 2), \
-  only screen and (min--moz-device-pixel-ratio: 2), \
-  only screen and (-o-min-device-pixel-ratio: 2/1), \
-  only screen and (min-device-pixel-ratio: 2), \
-  only screen and (min-resolution: 192dpi), \
-  only screen and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['large_retina'] = 'only screen ' +
+  'and (-webkit-min-device-pixel-ratio: 2) and (min-width: 64em), ' +
+  'only screen and (min--moz-device-pixel-ratio: 2) and (min-width: 64em), ' +
+  'only screen and (-o-min-device-pixel-ratio: 2/1) and (min-width: 64em), ' +
+  'only screen and (min-device-pixel-ratio: 2) and (min-width: 64em), ' +
+  'only screen and (min-resolution: 192dpi) and (min-width: 64em), ' +
+  'only screen and (min-resolution: 2dppx) and (min-width: 64em)';
 
 $(document).foundation();
